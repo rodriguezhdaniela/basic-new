8a31252f381820b02fc70f05cb7adb7bdffec7d8
Daniela Rodriguez <rodriguezh.daniela@gmail.com>
2020-04-07 12:07:39 -0500

    controller and request

---
+++ app/Http/Controllers/Backend/PostController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use App\Http\Requests\PostRequest;
+
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::latest()->get();
+
+        return response()->view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return response()->view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+
+        $post = Post::create([
+            'user_id' => auth()->user()->id
+        ] + $request->all());
+
+        //image
+        if($request->file('file')){
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+        }
+
+        //retornar
+        return back()->with('status', 'Creado con éxito');
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        dd($request);
+        $post->update($request->all());
+
+        if($request->file('file')) {
+            //eliminar imagen
+            Storage::disk('public')->delete($post->image);
+
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+
+        }
+
+        return back()->with('status', 'Actualizado con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //Eliminación imagen
+        Storage::disk('public')->delete($post->image);
+
+        $post->delete();
+
+        return back()->with('status', 'Eliminado con éxito');
+    }
+}
